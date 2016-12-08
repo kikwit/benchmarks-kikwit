76e82c2fc257c9b7f28d7fa6467a6f48f5762b4d
elondo <mbonze.elondo@gmail.com>
2016-12-08 17:07:03 +0000

    Updates for Kikwit.

--- kikwit/app.js
+++ kikwit/app.js
@@ -7,15 +7,8 @@ const server = new Server();
 server.configure(config => {
 
     config.addJavaScriptFile('config/default.js');
-    config.addJavaScriptFile(`config/${config.environment}.js`, true);    
- 
-    if (config.isEnvironment('development')) {
-        config.addUserConfig();
-    }
-    
-    config.addEnvironmentVariables();  
 });
 
 server.start().then(() => {
-    console.log(`Online, PID: ${process.pid}`);
+    console.log('kikwit - Server started');
 });
--- kikwit/config/default.js
+++ kikwit/config/default.js
@@ -1,20 +1,7 @@
-import consolidate from 'consolidate';
-
 export default {
             
-    cluster: false,
-    route: {
-        overview: true  
-    },
     server: {
         port: 3000
-    },
-    views: {
-        cache: false,
-        defaultEngine: 'dust',
-        engines: {
-            dust: consolidate['dust']
-        }
     }
 }
 
